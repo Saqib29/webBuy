f5368b9fa9fbf9859f7ddf8e3a523794696d385e
Saqib <saqib29111@gmail.com>
2021-03-13 01:30:38 +0600

    project reloaded

--- webBuy/Models/User.cs
+++ webBuy/Models/User.cs
@@ -11,13 +11,10 @@ namespace webBuy.Models
 {
     using System;
     using System.Collections.Generic;
-    using System.ComponentModel.DataAnnotations;
     
-
     public partial class User
     {
         public int userId { get; set; }
-        //[Required]
         public string email { get; set; }
         public string name { get; set; }
         public string password { get; set; }
