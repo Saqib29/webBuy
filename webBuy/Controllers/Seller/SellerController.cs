d67b03d6aa54aac7880ca7ea0b88bdd4b350b7da
Saqib <saqib29111@gmail.com>
2021-03-13 02:23:02 +0600

    Seller page + controller add

---
+++ webBuy/Controllers/Seller/SellerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace webBuy.Controllers.Seller
+{
+    public class SellerController : Controller
+    {
+        // GET: Seller
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
