d80e3e59105861a54cbff8fe0d73a27510296e78
Saqib <saqib29111@gmail.com>
2021-03-12 22:09:21 +0600

    register page validation complete

--- webBuy/Models/UserPartial.cs
+++ webBuy/Models/UserPartial.cs
@@ -9,5 +9,10 @@ namespace webBuy.Models
     [MetadataType(typeof(UserMetaData))]
     public partial class User
     {
+        [Required(ErrorMessage = "Confirm messaeg required")]
+        [StringLength(50, ErrorMessage = "Must be at least 6 characters", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Compare("password", ErrorMessage = "Password doesn't match")]
+        public string confirmPassword { get; set; }
     }
 }
