eca2aa3214d0692a80645bb90c338bd21e7a1833
Saqib <saqib29111@gmail.com>
2021-03-14 03:22:48 +0600

    create product

---
+++ webBuy/Controllers/Seller/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace webBuy.Controllers.Seller
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
