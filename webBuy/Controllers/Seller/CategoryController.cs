acc81931c7720a0b3742b97669061163de4f7db3
Saqib <saqib29111@gmail.com>
2021-03-14 19:04:23 +0600

    category view page

---
+++ webBuy/Controllers/Seller/CategoryController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using webBuy.Models;
+using webBuy.Repositories;
+
+namespace webBuy.Controllers.Seller
+{
+    public class CategoryController : Controller
+    {
+        CategoryRepository categoryRepository = new CategoryRepository();
+        public ActionResult Index()
+        {
+            ViewBag.category =  categoryRepository.GetAll() as List<Category>;
+            if(ViewBag.category.Count == 0)
+            {
+                TempData["msg"] = "No categories! please create category first";
+                return RedirectToAction("Index", "Seller");
+            }
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return Content("Category Create Page");
+        }
+    }
+}
